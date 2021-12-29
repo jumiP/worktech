a99a836283613d40e8dcc9bdad50f9868dc3c047
eunhye3333 <eunhye_3@naver.com>
2021-12-29 14:52:33 +0900

    Member vo 추가

---
+++ worktech_final/src/main/java/com/groupware/worktech/member/model/vo/Member.java
@@ -0,0 +1,144 @@
+package com.groupware.worktech.member.model.vo;
+
+import java.sql.Date;
+
+public class Member {
+
+	private String mNo;
+	private String email;
+	private String name;
+	private String pwd;
+	private String jobGrade;
+	private Date birthDay;
+	private String phone;
+	private String Address;
+	private String mGrade; // 회원 등급 : user/admin
+	private int dNo;
+	private Date hireDate;
+	private String mStatus;
+	
+	public Member() {}
+
+	public Member(String mNo, String email, String name, String pwd, String jobGrade, Date birthDay, String phone,
+			String address, String mGrade, int dNo, Date hireDate, String mStatus) {
+		super();
+		this.mNo = mNo;
+		this.email = email;
+		this.name = name;
+		this.pwd = pwd;
+		this.jobGrade = jobGrade;
+		this.birthDay = birthDay;
+		this.phone = phone;
+		Address = address;
+		this.mGrade = mGrade;
+		this.dNo = dNo;
+		this.hireDate = hireDate;
+		this.mStatus = mStatus;
+	}
+
+	public String getmNo() {
+		return mNo;
+	}
+
+	public void setmNo(String mNo) {
+		this.mNo = mNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getJobGrade() {
+		return jobGrade;
+	}
+
+	public void setJobGrade(String jobGrade) {
+		this.jobGrade = jobGrade;
+	}
+
+	public Date getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(Date birthDay) {
+		this.birthDay = birthDay;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getmGrade() {
+		return mGrade;
+	}
+
+	public void setmGrade(String mGrade) {
+		this.mGrade = mGrade;
+	}
+
+	public int getdNo() {
+		return dNo;
+	}
+
+	public void setdNo(int dNo) {
+		this.dNo = dNo;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getmStatus() {
+		return mStatus;
+	}
+
+	public void setmStatus(String mStatus) {
+		this.mStatus = mStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [mNo=" + mNo + ", email=" + email + ", name=" + name + ", pwd=" + pwd + ", jobGrade=" + jobGrade
+				+ ", birthDay=" + birthDay + ", phone=" + phone + ", Address=" + Address + ", mGrade=" + mGrade
+				+ ", dNo=" + dNo + ", hireDate=" + hireDate + ", mStatus=" + mStatus + "]";
+	}
+
+	
+	
+}

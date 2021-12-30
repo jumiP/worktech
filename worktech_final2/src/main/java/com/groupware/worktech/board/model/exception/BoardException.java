4643c56c2143cf76bfbedeea4bcb4eab7bbe6224
eunhye3333 <eunhye_3@naver.com>
2021-12-30 09:32:54 +0900

    불필요한 폴더 제거

--- worktech_final2/src/main/java/com/groupware/worktech/board/model/exception/BoardException.java
+++
@@ -1,9 +0,0 @@
-package com.groupware.worktech.board.model.exception;
-
-public class BoardException extends RuntimeException {
-	public BoardException() {}
-	
-	public BoardException(String msg) {
-		super(msg);
-	}
-}

b4ed2ca7086a2750fb7345e52755169f818ff843
eunhye3333 <eunhye_3@naver.com>
2021-12-31 09:11:28 +0900

    관리자 예약 자산 관리 진행 중

--- worktech_final/src/main/java/com/groupware/worktech/admin/model/dao/AdminDAO.java
+++ worktech_final/src/main/java/com/groupware/worktech/admin/model/dao/AdminDAO.java
@@ -2,11 +2,14 @@ package com.groupware.worktech.admin.model.dao;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.groupware.worktech.admin.model.vo.Department;
 import com.groupware.worktech.admin.model.vo.RvProduct;
+import com.groupware.worktech.admin.model.vo.RvRange;
+import com.groupware.worktech.common.PageInfo;
 
 @Repository("aDAO")
 public class AdminDAO {
@@ -106,10 +109,38 @@ public class AdminDAO {
 		return sqlSession.insert("adminMapper.insertRvProduct", rp);
 	}
 
-	public int insertRvRange(SqlSessionTemplate sqlSession, RvProduct rp) {
+	public int insertRvRange(SqlSessionTemplate sqlSession, ArrayList<RvRange> rrList) {
+		int result = 0;
 		
+		for(RvRange r: rrList) {
+			result += sqlSession.insert("adminMapper.insertRvRange", r);
+		}
 		
-		return 0;
+		return result;
+	}
+
+	public int getRvListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("adminMapper.getRvListCount");
+	}
+
+	public ArrayList<RvProduct> selectRvProductList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		
+		return (ArrayList)sqlSession.selectList("adminMapper.selectRvProductList", null, rowBounds);
+	}
+
+	public RvProduct selectRvProduct(SqlSessionTemplate sqlSession, int pdNo) {
+		return sqlSession.selectOne("adminMapper.selectRvProduct", pdNo);
+	}
+
+	public int deleteRvProduct(SqlSessionTemplate sqlSession, int pdNo) {
+		return sqlSession.update("adminMapper.deleteRvProduct", pdNo);
+	}
+
+	public ArrayList<Integer> getOriginRvRangeList(SqlSessionTemplate sqlSession, int pdNo) {
+		return (ArrayList)sqlSession.selectList("adminMapper.getOriginRvRangeList", pdNo);
 	}
 
 }

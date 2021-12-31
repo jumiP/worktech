9c8bc39da7330948a354721f4e9247c36d809694
eunhye3333 <eunhye_3@naver.com>
2021-12-31 17:34:29 +0900

    채팅 구현 중

--- worktech_final/src/main/java/com/groupware/worktech/chat/common/ChattingHandler.java
+++ worktech_final/src/main/java/com/groupware/worktech/chat/common/ChattingHandler.java
@@ -1,49 +1,15 @@
 package com.groupware.worktech.chat.common;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.socket.CloseStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import lombok.extern.log4j.Log4j;
-
-@Log4j
+ 
+@Controller
 public class ChattingHandler extends TextWebSocketHandler {
-	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
-	private Logger log = LoggerFactory.getLogger(ChattingHandler.class);
-	
 	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		
-		log.info("#ChattingHandler, afterConnectionEstablished");
-		sessionList.add(session);
-		
-		log.info(session.getPrincipal().getName() + "님이 입장하셨습니다.");
-	}
-	
-	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		
-		log.info("#ChattingHandler, handleMessage");
-		log.info(session.getId() + ": " + message);
-		
-		for(WebSocketSession s : sessionList) {
-			s.sendMessage(new TextMessage(session.getPrincipal().getName() + ":" + message.getPayload()));
-		}
-	}
-	
-	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		
-		log.info("#ChattingHandler, afterConnectionClosed");
-
-		sessionList.remove(session);
-		
-		log.info(session.getPrincipal().getName() + "님이 퇴장하셨습니다.");
-	}
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
 }
+

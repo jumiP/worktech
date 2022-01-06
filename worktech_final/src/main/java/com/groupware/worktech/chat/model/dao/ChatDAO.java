88265c9dd56cd5f46e1c1db1c7ed661827f4a7e8
NaEunHye <eunhye_3@naver.com>
2022-01-06 09:20:56 +0900

    채팅 목록 구현

--- worktech_final/src/main/java/com/groupware/worktech/chat/model/dao/ChatDAO.java
+++ worktech_final/src/main/java/com/groupware/worktech/chat/model/dao/ChatDAO.java
@@ -1,17 +1,53 @@
 package com.groupware.worktech.chat.model.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.groupware.worktech.admin.model.vo.Department;
+import com.groupware.worktech.chat.model.vo.ChatMessage;
 import com.groupware.worktech.chat.model.vo.ChatRoom;
+import com.groupware.worktech.member.model.vo.Member;
 
 @Repository("cDAO")
 public class ChatDAO {
 
 	public ArrayList<ChatRoom> selectChatList(SqlSessionTemplate sqlSession, String mNo) {
 		return (ArrayList)sqlSession.selectList("chatMapper.selectChatList", mNo);
+	}
+
+	public ArrayList<Department> getChatDepartmentList(SqlSessionTemplate sqlSession) {
+		return (ArrayList)sqlSession.selectList("chatMapper.getChatDepartmentList");
+	}
+
+	public ArrayList<Member> selectDeptMember(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
+		return (ArrayList)sqlSession.selectList("chatMapper.selectDeptMember", map);
+	}
+
+	public ArrayList<String> selectMemberName(SqlSessionTemplate sqlSession, ArrayList<String> mNoes) {
+		return (ArrayList)sqlSession.selectList("chatMapper.selectMemberName", mNoes);
+	}
+
+	public int insertGroupChatroom(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
+		return sqlSession.insert("chatMapper.insertGroupChatroom", map);
+	}
+
+	public int insertGroupChatList(SqlSessionTemplate sqlSession, String mNo) {
+		return sqlSession.insert("chatMapper.insertGroupChatList", mNo);
+	}
+
+	public ChatRoom selectCreateChatRoom(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("chatMapper.selectCreateChatRoom");
+	}
+
+	public ArrayList<ChatMessage> selectMessageList(SqlSessionTemplate sqlSession, int chatRoomNo) {
+		return (ArrayList)sqlSession.selectList("chatMapper.selectMessageList", chatRoomNo);
+	}
+
+	public int getNotReadCount(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
+		return sqlSession.selectOne("chatMapper.getNotReadCount", map);
 	}
 
 }
--- worktech_final/src/main/java/com/groupware/worktech/chat/model/service/ChatService.java
+++ worktech_final/src/main/java/com/groupware/worktech/chat/model/service/ChatService.java
@@ -1,13 +1,17 @@
 package com.groupware.worktech.chat.model.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.groupware.worktech.admin.model.vo.Department;
 import com.groupware.worktech.chat.model.dao.ChatDAO;
+import com.groupware.worktech.chat.model.vo.ChatMessage;
 import com.groupware.worktech.chat.model.vo.ChatRoom;
+import com.groupware.worktech.member.model.vo.Member;
 
 @Service("cService")
 public class ChatService {
@@ -20,5 +24,43 @@ public class ChatService {
 
 	public ArrayList<ChatRoom> selectChatList(String mNo) {
 		return cDAO.selectChatList(sqlSession, mNo);
+	}
+
+	public ArrayList<Department> getChatDepartmentList() {
+		return cDAO.getChatDepartmentList(sqlSession);
+	}
+
+	public ArrayList<Member> selectDeptMember(HashMap<String, String> map) {
+		return cDAO.selectDeptMember(sqlSession, map);
+	}
+
+	public ArrayList<String> selectMemberName(ArrayList<String> mNoes) {
+		return cDAO.selectMemberName(sqlSession, mNoes);
+	}
+
+	public int insertGroupChatroom(HashMap<String, String> map) {
+		return cDAO.insertGroupChatroom(sqlSession, map);
+	}
+
+	public int insertGroupChatList(ArrayList<String> mNoes) {
+		int result = 0;
+		
+		for(String mNo : mNoes) {
+			result += cDAO.insertGroupChatList(sqlSession, mNo);
+		}
+		
+		return result;
+	}
+
+	public ChatRoom selectCreateChatRoom() {
+		return cDAO.selectCreateChatRoom(sqlSession);
+	}
+
+	public ArrayList<ChatMessage> selectMessageList(int chatRoomNo) {
+		return cDAO.selectMessageList(sqlSession, chatRoomNo);
+	}
+
+	public int getNotReadCount(HashMap<String, String> map) {
+		return cDAO.getNotReadCount(sqlSession, map);
 	}
 }
